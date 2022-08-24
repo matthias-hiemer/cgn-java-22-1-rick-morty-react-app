b63b1462123aefe91a34ed940150094accf2c57d
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-08-24 12:10:21 +0200

    Refactor to custom hook useCharacter

---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react";
+import {Character} from "../model/Character";
+import axios from "axios";
+
+
+export default function useCharacter(id : string | undefined){
+
+    const [character, setCharacter] = useState<Character>()
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character/" + id)
+            .then(response => response.data)
+            .then(data => setCharacter(data))
+            .catch(console.error)
+    }, [id])
+
+    return character
+}
--- src/pages/CharacterDetailsPage.tsx
+++ src/pages/CharacterDetailsPage.tsx
@@ -1,21 +1,13 @@
 import {useParams} from "react-router-dom";
-import axios from "axios";
-import {useEffect, useState} from "react";
-import {Character} from "../model/Character";
 import CharacterCardDetail from "../components/CharacterCardDetail";
+import useCharacter from "../hooks/useCharacter";
 
 export default function CharacterDetailsPage() {
 
-    const [character, setCharacter] = useState<Character>()
     const params = useParams()
     const id = params.id
 
-    useEffect(() => {
-        axios.get("https://rickandmortyapi.com/api/character/" + id)
-            .then(response => response.data)
-            .then(data => setCharacter(data))
-            .catch(console.error)
-    }, [])
+    const character = useCharacter(id);
 
     return (
         <div>
